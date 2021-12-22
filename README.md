34762920294ba078ff9afb810ecc3129529d3888
Anton Delgado <johndoe@example.com>
2021-12-22 15:31:59 +0000

    vs code container

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # example_flask_docker
 
 I followed the guide [Here](https://dev.to/pacheco/dockerize-a-flask-app-and-debug-with-vscode-34i1) to create a dynamically refreshing Flask app in Docker.
+
+To execute the app, run 
+
+> python -m flask run --port 9000 --no-debugger --no-reload
--- api/__init__.py
+++ api/__init__.py
@@ -1,21 +1,20 @@
 from flask import Flask, jsonify
 import flask
-# import gunicorn
+import gunicorn
 import sys
-
-
+ 
 def create_app():
     app = Flask(__name__)
 
     @app.route("/api/test", methods=["GET"])
     def sample_route():
-        return jsonify({"message": "This is a sample route2"})
+        return jsonify({"message": "This is a sample route3"})
 
     @app.route("/", methods=["GET"])
     def sample_route2():
-        return jsonify({"message": "This is a sample route2",
+        return jsonify({"message": "This is a sample route4",
         "flaskversion":str(flask.__version__)
-        # ,"gunicornversion":str(gunicorn.__version__)
+        ,"gunicornversion":str(gunicorn.__version__)
         ,"PythonVersion":str(sys.version)
         
         })
